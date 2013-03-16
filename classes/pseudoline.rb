c18ec5abad0fa66ada46e3ec91036eeadd8e9adb
Luke Blaney <git@lukeblaney.co.uk>
2013-03-16 08:56:21 +0000

    fix pseudo lines so that get_info doesn't return station objects in its output

--- classes/pseudoline.rb
+++ classes/pseudoline.rb
@@ -8,12 +8,21 @@ class PseudoLine < Line
 		super(network, code, name)
 	end
 	def force_update
-		@info = {
+		info = {
 			:stops => @stops,
-			:stations => @stations,
+			:stations => {},
 			:destinations => {},
 			:lines => { @code => @name },
 		}
+		@stations.each_pair do |stationcode, station|
+			stationinfo = station.get_info
+			info[:stations][stationcode] = {
+				:n => stationinfo[:name],
+				:p => stationinfo[:platforms],
+				:l => stationinfo[:lines].sort,
+			}
+		end
+		@info = info
 	end
 	def add_station(code, station)
 		@stations[code] = station
