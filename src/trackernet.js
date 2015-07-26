cff9fb688070ca4af28ab36616cfc37bbfa50d56
Luke Blaney <git@lukeblaney.co.uk>
2015-07-26 20:10:49 +0100

    update TrackerNet Line Statuses every 5 mins

--- src/trackernet.js
+++ src/trackernet.js
@@ -2,22 +2,26 @@ var req = require('request');
 var xml = require('xml2js');
 var Route = require('./classes/route');
 function start() {
-	req("http://cloud.tfl.gov.uk/TrackerNet/LineStatus", processlines)
+	setInterval(processlines, 300000);
+	processlines();
 }
 
-function processlines(err, resp, rawbody) {
-	if (err) {
-		console.err(err);
-		return;
-	}
-	xml.parseString(rawbody, function(err, body){
-		body.ArrayOfLineStatus.LineStatus.forEach(function (linestatus) {
-			var data = {
-				name: linestatus.Line[0].$.Name,
-				status:linestatus.Status[0].$.Description,
-				details:linestatus.$.StatusDetails,
-			}
-			Route.update("TN-"+getLineCode(data.name), data);
+function processlines() {
+	console.log("Updating Line Statuses from TrackerNet");
+	req("http://cloud.tfl.gov.uk/TrackerNet/LineStatus", function processlines(err, resp, rawbody) {
+		if (err) {
+			console.err(err);
+			return;
+		}
+		xml.parseString(rawbody, function(err, body){
+			body.ArrayOfLineStatus.LineStatus.forEach(function (linestatus) {
+				var data = {
+					name: linestatus.Line[0].$.Name,
+					status:linestatus.Status[0].$.Description,
+					details:linestatus.$.StatusDetails,
+				}
+				Route.update("TN-"+getLineCode(data.name), data);
+			});
 		});
 	});
 }
