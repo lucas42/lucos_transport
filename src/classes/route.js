15f995b9568f2b14dc16b437f56e55c2b3252783
Luke Blaney <git@lukeblaney.co.uk>
2015-07-28 02:22:42 +0100

    apply abstracted thing logic to route class

--- src/classes/route.js
+++ src/classes/route.js
@@ -1,32 +1,6 @@
-routes = {};
-function Route(id) {
-	var data = {};
-	routes[id] = this;
-	this.setData = function (newdata) {
-		data = newdata;
-	};
-	this.getRawData = function () {
-		var output = {};
-		for (i in data) {
-			output[i] = data[i];
-		}
-		return output;
-	}
-	this.getId = function getId() {
-		return id;
-	}
-	var lastRefresh = null;
-	this.attemptRefresh = function attemptRefresh(callback) {
-		if (!this.refresh || Date.now() - lastRefresh < 30000) {
-			callback();
-			return;
-		}
-		this.refresh.call(this, function () {
-			console.log("Refreshing "+data.title);
-			lastRefresh = Date.now();
-			callback();
-		});
-	};
+Thing = require('./thing');
+function Route() {
+	Thing.apply(this, arguments);
 	var stops = {};
 	this.addStop = function addStop(stop) {
 		stops[stop.getId()] = stop;
@@ -36,7 +10,9 @@ function Route(id) {
 		for (i in stops) output.push(stops[i]);
 		return output;
 	}
+
 }
+Thing.extend(Route);
 Route.prototype.getData = function getData() {
 	var output = this.getRawData();
 	output.link = "/route/"+this.getId();
@@ -44,24 +20,4 @@ Route.prototype.getData = function getData() {
 	return output;
 }
 
-Route.update = function update(id, data) {
-	var route;
-	if (id in routes) {
-		route = routes[id];
-	} else {
-		route = new Route(id);
-	}
-	route.setData(data);
-	return route;
-}
-Route.getById = function getById(id) {
-	if (id in routes) {
-		return routes[id];
-	}
-	return null;
-}
-Route.getAll = function getAll() {
-	return routes;
-}
-
-module.exports = Route
+module.exports = Route;
