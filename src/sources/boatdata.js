b119ceacce1c3a8876213300a5949d0b9504f6a9
Luke Blaney <git@lukeblaney.co.uk>
2017-02-26 23:31:09 +0000

    add rb1x to list of boat lines

--- src/sources/boatdata.js
+++ src/sources/boatdata.js
@@ -12,7 +12,7 @@ function loadData() {
 	// The same boats can serve any route in the network.
 	// Might need to rethink the data model, but for now just 
 	// hardcode the relevant routes
-	["rb1","rb2","rb4","rb5","rb6"].forEach(function (id){
+	["rb1","rb1x","rb2","rb4","rb5","rb6"].forEach(function (id){
 		routes.push(new Route(network, id));
 	});
 	for (var id in boats) {
