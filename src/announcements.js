6ff074c4820b635443e6ee465380ec28cea05447
Luke Blaney <git@lukeblaney.co.uk>
2017-04-15 12:26:20 +0100

    make stop announcements more detailed

--- src/announcements.js
+++ src/announcements.js
@@ -33,14 +33,48 @@ switch(document.body.dataset.classtype) {
 	case "Stop":
 		Pubsub.listen('eventApproaching', function (data) {
 			if (document.body.dataset.classid != data.stop.classID) return;
-			speak("The next "+data.vehicle.vehicleType+" arriving at "+data.platform.simpleName+" is for "+data.vehicle.simpleDestination);
+			var text = "The next "+data.vehicle.vehicleType+" at "+data.platform.simpleName+" will be ";
+			text += (data.vehicle.routeName.charAt(0).match(/[aoeui]/)) ? "an " : "a ";
+			text += data.vehicle.routeName+" "+data.vehicle.vehicleType;
+			if (data.vehicle.simpleDestination) text += " to "+fixStationName(data.vehicle.simpleDestination);
+			speak(text);
+		});
+		Pubsub.listen('eventArrived', function (data) {
+			if (document.body.dataset.classid != data.stop.classID) return;
+			var text = "The "+data.vehicle.vehicleType+" at "+data.platform.simpleName+" is ";
+			text += (data.vehicle.routeName.charAt(0).match(/[aoeui]/)) ? "an " : "a ";
+			text += data.vehicle.routeName+" "+data.vehicle.vehicleType;
+			if (data.vehicle.simpleDestination) text += " to "+fixStationName(data.vehicle.simpleDestination);
+			speak(text);
 		});
 		break;
 	case "Vehicle":
 		Pubsub.listen('eventApproaching', function (data) {
 			if (document.body.dataset.classid != data.vehicle.classID) return;
-			speak("The next stop is "+data.stop.simpleName);
+			speak("The next stop is "+fixStationName(data.stop.simpleName));
+		});
+		Pubsub.listen('eventArrived', function (data) {
+			if (document.body.dataset.classid != data.vehicle.classID) return;
+			speak("This is "+fixStationName(data.stop.simpleName));
 		});
 		break;
 }
 
+/**
+ * Substitues certain strings used in Station Names with something more pronouncable
+ * @param name {String} The Name of a station, or a destination
+ */
+function fixStationName(name) {
+	if (!name) return 
+	return name
+	.replace("via T4 Loop", "Terminal 4")
+	.replace("1-2-3", "1, 2 and 3")
+	.replace("123 + 5", "1, 2, 3 and 5")
+	.replace("CX", "Charing Cross")
+	.replace("Arsn", "Arsenal")
+	.replace(" St ", " Street ")
+	.replace("Southwark", "Suthirck")
+	.replace("Fulham", "Fullam")
+	.replace(/int$/i, "International")
+	.replace(/\(.*\)/, "");
+}
