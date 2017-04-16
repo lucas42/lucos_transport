86a50f2defb7937c63af19eed0458f962ec3a5bc
Luke Blaney <git@lukeblaney.co.uk>
2017-04-16 02:51:21 +0100

    add test for serialisation

---
+++ tests/serialised.js
@@ -0,0 +1,34 @@
+import test from 'ava';
+const Network = require("../src/classes/network"),
+	Route = require("../src/classes/route");
+
+test('Serialised Network', test => {
+	var network1 = new Network('net1');
+	network1.setField('fieldname', 'fieldval');
+	new Route(network1, 'route1');
+	new Route(network1, 'route2');
+	var network2 = new Network('net2');
+	new Route(network2, 'route3');
+	test.deepEqual(Network.getAllSerialised(), {
+		'net1': {
+			'classID': 'Network-net1',
+			'classType': 'Network',
+			'fieldname': 'fieldval',
+			'relations': {
+				'route': [
+					[['net1'], 'route1'],
+					[['net1'], 'route2']
+				],
+			}
+		},
+		'net2': {
+			'classID': 'Network-net2',
+			'classType': 'Network',
+			'relations': {
+				'route': [
+					[['net2'], 'route3']
+				],
+			}
+		}
+	});
+});
