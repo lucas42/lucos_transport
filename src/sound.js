cd476f5e564a7f71102f4d2273d4d1b1972dac1d
Luke Blaney <git@lukeblaney.co.uk>
2025-01-21 18:28:04 +0000

    Replace mespeak library with Web Speech API (Annnouncements still don't properly play, as the times aren't being updated, and hence events don't fire)

--- src/sound.js
+++ src/sound.js
@@ -5,14 +5,15 @@ var currentExtraData;
 
 // Lazy load sound, only when needed
 function preload() {
-	/*const speech = require("mespeak");
-
-	// Setup voice module
-	speech.loadConfig(require("mespeak/src/mespeak_config.json"));
-	speech.loadVoice(require("mespeak/voices/en/en-rp.json"));
-
 	var queue = [];
 	var isSpeaking = false;
+
+	let preferredVoice;
+	window.speechSynthesis.addEventListener("voiceschanged", () => {
+		// Pick a voice for speech - choose one in British English, that doesn't require any remote calls, with a preference for "Martha"
+		preferredVoice = window.speechSynthesis.getVoices().filter(voice => voice.lang === 'en-GB' && voice.localService).sort((a,b) => a.name=="Martha" && -1).shift();
+		console.log("voice loaded", preferredVoice);
+	});
 	speak = function speak(message) {
 		if (!soundIsEnabled()) return;
 		if (isSpeaking) {
@@ -20,13 +21,24 @@ function preload() {
 			return;
 		}
 		isSpeaking = true;
-		speech.speak(message, {}, function doneSpeaking() {
+		const utterance = new SpeechSynthesisUtterance(message);
+		if (preferredVoice) utterance.voice = preferredVoice;
+		utterance.addEventListener('end', () => {
 			isSpeaking = false;
 			if (queue.length) {
 				speak(queue.shift());
 			}
 		});
-	}*/
+		utterance.addEventListener('error', event => {
+			isSpeaking = false;
+			if (event.error == 'not-allowed') {
+				console.log("//TODO: automatically make sound button disabled")
+			} else {
+				console.error("utterance hit unknown error", event.error);
+			}
+		})
+		window.speechSynthesis.speak(utterance);
+	}
 }
 function load() {
 
@@ -43,6 +55,7 @@ function soundIsEnabled() {
 export default {
 	enable: () => {
 		localStorage.setItem("soundEnabled", true);
+		speak("Announcents On");
 		setTimeout(load, 0);
 	},
 	disable: () => {
